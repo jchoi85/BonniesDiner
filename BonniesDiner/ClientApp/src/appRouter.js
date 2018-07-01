8698c59861d396273cec55bc1f329d85c24cd6e9
Kevin Jaturapatporn <kevin226j@gmail.com>
2018-06-30 22:51:35 -0700

    22:51

--- BonniesDiner/ClientApp/src/appRouter.js
+++ BonniesDiner/ClientApp/src/appRouter.js
@@ -1,8 +1,7 @@
 ﻿import React from 'react';
 import { Router, Route, browserHistory, IndexRoute } from "react-router";
 import { HomePageContainer } from "./components/public/homePageContainer";
-import { Layout } from './components/Layout';
-
+import { Layout } from "../src/components/Layout";
 
 export const AppRouter = () => {
         return (
