4977ed231ed08d1a9556c2e63891e5b4877f262c
Kevin Jaturapatporn <kevin226j@gmail.com>
2018-07-01 01:59:51 -0700

    1:59

--- BonniesDiner/ClientApp/src/appRouter.js
+++ BonniesDiner/ClientApp/src/appRouter.js
@@ -2,12 +2,14 @@
 import { Router, Route, browserHistory, IndexRoute } from "react-router";
 import { HomePageContainer } from "./components/public/homePageContainer";
 import { Layout } from "../src/components/Layout";
+import { ViewReportsContainer } from "../src/components/Staff/ViewReportsContainer";
 
 export const AppRouter = () => {
         return (
             <Router history={browserHistory}>
                 <Route path="/" component={Layout}>
                     <IndexRoute component={HomePageContainer} />
+                    <Route path="ViewReports" component={ViewReportsContainer} />
                 </Route>
             </Router>
         )
